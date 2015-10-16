250a57d5fbb488dada04670eff659f41684926fa
Will Stamper <epmatsw@gmail.com>
2015-10-16 15:19:06 -0400

    First not-so-great shot at loading on scroll

--- src/InfiniteTable.js
+++ src/InfiniteTable.js
@@ -2,6 +2,8 @@
 const React = require('react');
 const {PropTypes} = React;
 
+const $ = require('jquery');
+
 const InfiniteTable = React.createClass({
   displayName: 'InfiniteTable',
   propTypes: {
@@ -20,6 +22,8 @@ const InfiniteTable = React.createClass({
     loading: PropTypes.bool,
     loadingMessage: PropTypes.node,
     noValuesMessage: PropTypes.node,
+    bulkLoad: PropTypes.bool,
+    loadData: PropTypes.func,
   },
   getDefaultProps: function getDefaultProps() {
     return {
@@ -28,9 +32,59 @@ const InfiniteTable = React.createClass({
   },
   getInitialState: function getInitialState() {
     return {
-      topIndex: 0,
-      bottomIndex: this.props.pageLength - 1,
+      topChunk: 0,
+      bottomChunk: 2,
+      initialLoading: true,
     };
+  },
+  componentDidMount: function componentDidMount() {
+    this.props.loadData(0, this.props.pageLength * 3);
+  },
+  componentDidUpdate: function componentDidUpdate() {
+    // if (prevState.topIndex !== this.state.topIndex || prevState.bottomIndex !== this.state.bottomIndex) {
+    //   this.scrollToIndex();
+    // }
+  },
+  scrollToIndex: function scrollToIndex() {
+    // implement later
+  },
+  resetData: function resetData() {
+    this.setState(this.getInitialState());
+  },
+  findVisibleChunks: function findVisibleChunks() {
+    const tbody = $(this.refs.body);
+    const scrollTop = tbody.scrollTop();
+    const scrollBottom = scrollTop + tbody.height();
+    const rows = tbody.children('tr');
+    const visibleChunks = [];
+    for (let chunkIndex = 0; chunkIndex <= this.state.bottomChunk - this.state.topChunk; chunkIndex++) {
+      const firstRow = rows[chunkIndex * this.props.pageLength];
+      const chunkTop = firstRow.scrollTop();
+      if (chunkTop >= scrollTop && chunkTop <= scrollBottom) {
+        visibleChunks.push(chunkIndex);
+      }
+    }
+    return visibleChunks;
+  },
+  handleScroll: function handleScroll() {
+    const {pageLength, loadData} = this.props;
+    const {topChunk, bottomChunk} = this.state;
+    const visibleChunks = this.findVisibleChunks();
+    // How to handle telling when there's no more to scroll?
+    if (visibleChunks[visibleChunks.length - 1] >= bottomChunk) {
+      loadData((bottomChunk + 1) * pageLength, (bottomChunk + 2) * pageLength);
+      this.setState({
+        topChunk: topChunk + 1,
+        bottomChunk: bottomChunk + 1,
+      });
+    } else if (topChunk > 0 && visibleChunks[0] <= topChunk) {
+      loadData((topChunk - 1) * pageLength, (topChunk - 0) * pageLength);
+      this.setState({
+        topChunk: topChunk - 1,
+        bottomChunk: bottomChunk - 1,
+      });
+    }
+    // Handle case where no chunks are visible?
   },
   _renderRow: function _renderRow(datum, index) {
     const Row = this.props.rowComponent;
@@ -41,7 +95,7 @@ const InfiniteTable = React.createClass({
   render: function render() {
     const {headerElement, footerElement, loading, data, loadingMessage, noValuesMessage, colCount} = this.props;
     return (
-      <table ref='table'>
+      <table ref='table' onScroll={this.handleScroll}>
        <thead>
          { headerElement }
        </thead>
