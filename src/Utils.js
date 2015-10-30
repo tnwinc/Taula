27083fe1b4de0874faad0b2e8f65bc30d7aae3f0
Will Stamper <epmatsw@gmail.com>
2015-10-30 10:20:43 -0700

    Reformat utils for eslint

--- src/Utils.js
+++ src/Utils.js
@@ -2,46 +2,33 @@
 const $ = require('jquery');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const domFromReact = function domFromReact(component) {
-  return $(ReactDOM.findDOMNode(component));
-};
-const {Children} = React;
-
-const {findRenderedComponentWithType} = require('react-addons-test-utils');
 
 let iframe = undefined;
-let div = undefined;
-module.exports = {
-  domFromReact,
-  setupForTest: function setupForTest(){
-    if (iframe){
-      if(div){
-        ReactDOM.unmountComponentAtNode(div);
-      }
-      document.body.removeChild(iframe);
-    }
+let parent = undefined;
 
-    iframe = document.createElement('iframe');
-    document.body.appendChild(iframe);
-    
-    div = document.createElement('div'); 
-    div.id = 'test_container';
-    
-    iframe.contentDocument.body.appendChild(div)
-  },
-  renderFromReactClass: function renderFromReactClass (claz, props, type){
-      let parent = div;
-      let element = React.createElement(claz, props);
-      let component = undefined;
-      if (type){
-        parent = document.createElement(type);
-        div.appendChild(parent);
-      }
-
-      component = ReactDOM.render(element, parent);
-
-      return {component, element, $domNode: $(ReactDOM.findDOMNode(component))};
-   },
+module.exports.domFromReact = function domFromReact(component) {
+  return $(ReactDOM.findDOMNode(component));
 };
 
+module.exports.setupForTest = function setupForTest() {
+  if (iframe) {
+    if (parent) {
+      ReactDOM.unmountComponentAtNode(parent);
+    }
+    document.body.removeChild(iframe);
+  }
 
+  iframe = document.createElement('iframe');
+  document.body.appendChild(iframe);
+};
+
+module.exports.renderFromReactClass = function renderFromReactClass(claz, props, type = 'div') {
+  const element = React.createElement(claz, props);
+
+  parent = document.createElement(type);
+  iframe.appendChild(parent);
+
+  const component = ReactDOM.render(element, parent);
+
+  return {component, element, $domNode: $(ReactDOM.findDOMNode(component))};
+};
